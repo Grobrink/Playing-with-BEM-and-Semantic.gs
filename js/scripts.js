fc36b42d5e70c3ab6b763ff84aedd97bed31389f
Pierre Lefèvre <karapost@gmail.com>
2014-12-06 10:44:42 +0100

    Update layout to fit better

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(function () {
 
 	// define params
-	var duration = $("#the-last").height() + $(window).height();
+	var parallaxElement = '#la-une';
+	var duration = $(parallaxElement).height() + $(window).height();
 	var bgPosMovement = "0 " + (duration*0.5) + "px";
 
 	// init controller
 	var controller = new ScrollMagic({globalSceneOptions: {triggerHook: "onLeave", duration: duration}});
 
 	// build scenes
-	new ScrollScene({triggerElement: "#the-last"})
-		.setTween(TweenMax.to("#the-last", 1, {backgroundPosition: bgPosMovement, ease: Linear.easeNone}))
+	new ScrollScene({triggerElement: parallaxElement})
+		.setTween(TweenMax.to(parallaxElement, 1, {backgroundPosition: bgPosMovement, ease: Linear.easeNone}))
 		.addTo(controller);
 		// .addIndicators({zindex: 1, suffix: "1"});
 });
